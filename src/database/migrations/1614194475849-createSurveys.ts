7729e20497d263a85f431497934c9cba116ff795
marcelo-s-correa <marcelo.s.correa10@gmail.com>
2021-02-24 17:00:15 -0300

    Segundo Commit

---
+++ src/database/migrations/1614194475849-createSurveys.ts
@@ -0,0 +1,38 @@
+import { query } from "express";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createSurveys1614194475849 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "surveys",
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                    },
+                    {
+                        name: 'title',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'description',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestemp',
+                        default: 'now()',
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('surveys')
+    }
+
+}
